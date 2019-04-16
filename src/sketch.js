32f55733f0c07a599dd02b9e3a2c2468f982394a
github username <github email address>
2019-04-16 10:29:38 -0400

    added background

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,21 @@
+// function setup() {
+//   createCanvas(800,400);
+//   createSprite(400, 200, 50, 50);
+// }
+//
+// function draw() {
+//   background(255,255,255);
+//   drawSprites();
+// }
+let bg;
+function preload() {
+  bg = loadImage('assets/cloud-bg.png')
+}
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1920,700)
 }
 
 function draw() {
-  background(255,255,255);
-  drawSprites();
+  background(bg)
 }
