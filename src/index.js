9cedb5ea329dcdb2c933583486c6b8bea09e11f6
github username <github email address>
2019-04-16 09:35:36 -0400

    changed repo

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const userUlTag = document.querySelector('.user-list')
+
+const userTag = (user) => {
+return `<li>
+  <h3>${user.name}
+    <button>Edit</button>
+  </h3>
+</li>`;
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const endPoint = 'http://localhost:3000/api/v1/users';
+  fetch(endPoint)
+    .then((response) => {
+      return response.json()
+     }).then((parsedJson) => {
+      parsedJson.forEach((user) => {
+        userUlTag.innerHTML += userTag(user)
+      })
+    })
+});
