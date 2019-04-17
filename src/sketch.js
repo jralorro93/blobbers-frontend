1f811c3fc0f0bb29e64865a63f0d67159dc496cc
merestrickland <mere.strickland@gmail.com>
2019-04-17 11:22:10 -0400

    big blob move

--- src/sketch.js
+++ src/sketch.js
@@ -22,6 +22,7 @@
 
 let stretchy;
 let blobbers;
+let enemies;
 
 function setup() {
 
@@ -29,15 +30,29 @@ function setup() {
 
 
 
-  ///////////////CREATING RANDOM BLOBS, (20 OF THEM)
+  ///////////////CREATING RANDOM BLOBS, (20 OF THEM, SAME COLOR)
   blobbers = new Group();
 
   for(let j=0; j<20; j++){
     let blob = createSprite(random(0, width), random(0, height), 10, 10);
-    blob.shapeColor = color(50, 60, 255)
+    blob.shapeColor = color(237, 205, 0);
+    // blob.ellipse(10, 10, 10, 10);
     // dot.addAnimation('normal', 'assets/small_circle0001.png', 'assets/small_circle0001.png');
     blobbers.add(blob);
   }
+
+
+///////////////CREATING RANDOM ENEMIES, (20 OF THEM, DIFFERENT COLOR)
+  enemies = new Group();
+
+  for(let j=0; j<20; j++){
+    let enemy = createSprite(random(0, width), random(0, height), 10, 10);
+    enemy.shapeColor = color(50, 60, 255)
+    // dot.addAnimation('normal', 'assets/small_circle0001.png', 'assets/small_circle0001.png');
+    enemies.add(enemy);
+  }
+
+
 
 
 
@@ -73,12 +88,9 @@ function setup() {
     rotate(radians(this.getDirection()));
     ellipse(0, 0, 100+this.getSpeed(), 100-this.getSpeed());
   };
-  stretchy.maxSpeed = 3;
+  stretchy.maxSpeed = 5;
 
-  // blobber.draw = function() {
-  //   fill(50, 60, 255);
-  //   ellipse(500,500,100,100)
-  // }
+
 }
 
 
@@ -87,16 +99,13 @@ function draw() {
   background(255, 255, 255);
 
   //mouse trailer, the speed is inversely proportional to the mouse distance
-  stretchy.velocity.x = (mouseX-stretchy.position.x)/10;
-  stretchy.velocity.y = (mouseY-stretchy.position.y)/10;
-
-  // if (stretchy.overlap(blobbers)) {
-  //  blob.remove()
-  // }
+  // stretchy.velocity.x = (mouseX-stretchy.position.x)/10;
+  // stretchy.velocity.y = (mouseY-stretchy.position.y)/10;
 
   stretchy.overlap(blobbers, collect);
-
+  stretchy.overlap(enemies, dontCollect);
   // stretchy.collide(walls);
+
 
   drawSprites();
 }
@@ -105,9 +114,45 @@ function draw() {
 
 
 
+//////////WHAT HAPPENS WHEN YOU COLLECT SOMETHING YOU DON'T WANT
+
+function dontCollect(collector, collected) {
+  collected.remove();
+  collector.scale -= .15;
+///////if stretchy scale becomes less than -0.05000000000000007, remove it and pop up with "GAME OVER"
+}
+
+
+//////////WHAT HAPPENS WHEN YOU COLLECT SOMETHING YOU WANT
 function collect(collector, collected) {
   //collector is another name for stretchy
   //collected is the sprite in the group collectibles that triggered
   //the event
   collected.remove();
+  collector.scale += .15;
+
+}
+
+//////////SETTING UP THE KEY COMMANDS: UP, DOWN, LEFT, RIGHT, SPACE FOR FULL STOP
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    stretchy.setSpeed(1, 0);
+    // stretchy.velocity.x = (stretchy.position.x)/10;
+  }
+  else if (keyCode == DOWN_ARROW) {
+    stretchy.setSpeed(1, 90);
+    // stretchy.velocity.y = (stretchy.position.y)/10;
+  }
+  else if (keyCode == LEFT_ARROW) {
+    stretchy.setSpeed(1, 180);
+    // stretchy.velocity.x = (stretchy.position.x)/10;
+  }
+  else if (keyCode == UP_ARROW) {
+    stretchy.setSpeed(1, 270);
+    // stretchy.velocity.y = (stretchy.position.y)/10;
+  }
+  else if (key == ' ') {
+    stretchy.setSpeed(0, 0);
+  }
+  return false;
 }
