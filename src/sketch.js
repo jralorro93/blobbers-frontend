2d35d5a092e7062c2f29a64bb5eb56ec1326adbd
merestrickland <mere.strickland@gmail.com>
2019-07-02 14:08:12 -0400

    test

--- src/sketch.js
+++ src/sketch.js
@@ -7,7 +7,7 @@ let blobbers;
 let enemies;
 let score = 0;
 let bg;
-let timer = 60;
+let timer = 30;
 let gameStart = false;
 // let input;
 // let button;
@@ -40,29 +40,17 @@ function setup() {
 
 ///////////////CREATING RANDOM ENEMIES, (50 OF THEM, DIFFERENT COLOR)
   enemies = new Group();
-  for(let j=0; j<50; j++){
+  for(let j=0; j<70; j++){
     let enemy = createSprite(random(0, width), random(0, height), 10, 10);
     enemy.shapeColor = color(50, 60, 255)
     enemies.add(enemy);
   }
 
-
-
-
-
-
-
+////////////Creates User blob
   stretchy = createSprite(700, 400, 80, 80);
 
 
-  // enemies.draw = function() { ellipse(0,0,10,10)
 
-
-  // face = loadImage('assets/face.png');
-
-////////////Creates User blob
-  stretchy = createSprite(400, 200, 80, 80);
-  
   stretchy.draw = function() {
 
     //Color:
@@ -79,24 +67,28 @@ function draw() {
   background(bg);
 
 /////////////ADDED TIMER//////////////////////////
-  text("Time: " + timer, 1300, 30)
+  textSize(24);
+  text("Seconds Remaining: " + timer, 1100, 30)
   if (frameCount % 60 == 0 && timer > 0) {
     timer --;
 
   }
 ////////////GAME OVER/////////////////////////////
-  if (timer == 0) {
+  if (timer == 0 || stretchy.scale === -0.05000000000000007) {
     textSize(150)
     text("GAME OVER", 300, height/2);
+    textSize(24);
+    text("Score: " + score, 13, 25)
+    blobbers.remove()
+    enemies.remove()
   }
+
   stretchy.overlap(blobbers, collect);
   stretchy.overlap(enemies, dontCollect);
 
-  // login()
-
+  // fallingRain(blobbers)
   fallingRain(enemies);
   drawSprites();
-  fill(0, 255, 255);
   textSize(24);
   text("Score: " + score, 13, 25)
 
@@ -122,19 +114,10 @@ function draw() {
 ///////////CREATE MORE ENEMIES, EVERYTIME YOU RUN INTO THEM
 function moreEnemies(sprite) {
 
-
-for(let j=0; j<3; j++){
-  let enemy = createSprite(random(0, width), random(0, height), 10, 10);
-  enemy.shapeColor = color(50, 60, 255)
-  enemy.addToGroup(enemies);
-}
-
   for(let j=0; j<3; j++){
-    let sprite = createSprite(random(0, width), random(0, height), 10, 10);
-    sprite.shapeColor = color(50, 60, 255)
-    enemies.add(sprite);
+    let enemy = createSprite(random(0, width), random(0, height), 10, 10);
+    enemy.shapeColor = color(50, 60, 255)
   }
-
 }
 
 
@@ -143,14 +126,9 @@ for(let j=0; j<3; j++){
 
 function dontCollect(collector, collected) {
   collected.remove();
-  moreEnemies(enemies);
   collector.scale -= .15;
 
-
-
-  moreEnemies(enemies);
-
-
+  // moreEnemies(enemies);
   // collected.add();
   score -= 1;
 
@@ -163,10 +141,12 @@ function collect(collector, collected) {
   //collector is another name for stretchy
   //collected is the sprite in the group collectibles that triggered
   //the event
-  collected.remove();
-  collector.scale += .15;
+      collected.remove();
+      score+= 3
+  if (collector.scale < 1) {
+    collector.scale += .15;
 
-  score+= 3
+  }
 }
 
 //////////FALLING BLUE RAIN
@@ -182,20 +162,20 @@ function fallingRain(sprite) {
 //////////SETTING UP THE KEY COMMANDS: UP, DOWN, LEFT, RIGHT, SPACE FOR FULL STOP
 function keyPressed() {
   if (keyCode == RIGHT_ARROW) {
-    stretchy.setSpeed(3, 0);
+    stretchy.setSpeed(5, 0);
     // stretchy.velocity.x = (stretchy.position.x)/10;
   }
   else if (keyCode == DOWN_ARROW) {
-    stretchy.setSpeed(3, 90);
+    stretchy.setSpeed(5, 90);
 
     // stretchy.velocity.y = (stretchy.position.y)/10;
   }
   else if (keyCode == LEFT_ARROW) {
-    stretchy.setSpeed(3, 180);
+    stretchy.setSpeed(5, 180);
     // stretchy.velocity.x = (stretchy.position.x)/10;
   }
   else if (keyCode == UP_ARROW) {
-    stretchy.setSpeed(3, 270);
+    stretchy.setSpeed(5, 270);
     // stretchy.velocity.y = (stretchy.position.y)/10;
   }
   else if (key == ' ') {
@@ -224,11 +204,11 @@ function keyPressed() {
 
 
 
-
-
-
-
-
+// let newEnemies;
+// newEnemies = new Group();
+// // enemy.addToGroup(enemies);
+// newEnemies.add(enemy)
+// stretchy.overlap(newEnemies, dontCollect)
 
 
 
@@ -241,3 +221,340 @@ function keyPressed() {
 //   w.shapeColor = color(0);
 //   walls.add(w);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// let stretchy;
+// let blobbers;
+// let enemies;
+// let score = 0;
+// let background;
+// let timer = 60;
+// let gameStart = false;
+// // let input;
+// // let button;
+// // let greeting;
+//
+//
+// function preload() {
+//   background = loadImage('assets/new-cloud.jpg')
+// }
+//
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   //
+//   // input = createInput();
+//   // button = createButton('Submit');
+//   //
+//   //
+//   //
+//
+//
+//
+//   ///////////////CREATING RANDOM BLOBS, (20 OF THEM, SAME COLOR)
+//   blobbers = new Group();
+//   for(let j=0; j<20; j++){
+//     let blob = createSprite(random(0, width), random(0, height), 10, 10);
+//     blob.shapeColor = color(237, 205, 0);
+//     blobbers.add(blob);
+//   }
+//
+//
+// ///////////////CREATING RANDOM ENEMIES, (50 OF THEM, DIFFERENT COLOR)
+//   enemies = new Group();
+//   for(let j=0; j<50; j++){
+//     let enemy = createSprite(random(0, width), random(0, height), 10, 10);
+//     enemy.shapeColor = color(50, 60, 255)
+//     enemies.add(enemy);
+//   }
+//
+//
+//
+//
+//
+//
+//
+//   // stretchy = createSprite(700, 400, 80, 80);
+//
+//
+//   // enemies.draw = function() { ellipse(0,0,10,10)
+//
+//
+//   // face = loadImage('assets/face.png');
+//
+// ////////////Creates User blob
+//   stretchy = createSprite(400, 200, 80, 80);
+//
+//   stretchy.draw = function() {
+//
+//     //Color:
+//     fill(237, 205, 0);
+//     //make the ellipse stretch in the sprite direction
+//     //proportionally to its speed
+//     rotate(radians(this.getDirection()));
+//     ellipse(0, 0, 100+this.getSpeed(), 100-this.getSpeed());
+//   };
+//   stretchy.maxSpeed = 10;
+// }
+//
+// function draw() {
+//   background(background);
+//
+// /////////////ADDED TIMER//////////////////////////
+//   text("Time: " + timer, 1300, 30)
+//   if (frameCount % 60 == 0 && timer > 0) {
+//     timer --;
+//
+//   }
+// ////////////GAME OVER/////////////////////////////
+//   if (timer == 0) {
+//     textSize(150)
+//     text("GAME OVER", 300, height/2);
+//   }
+//   stretchy.overlap(blobbers, collect);
+//   stretchy.overlap(enemies, dontCollect);
+//
+//   // login()
+//
+//   fallingRain(enemies);
+//   drawSprites();
+//
+//
+//   fill(0, 255, 255);
+//   textSize(24);
+//   text("Score: " + score, 13, 25)
+//
+// }
+//
+// ////////////////////////Create login
+// // function login() {
+// //   if (gameStart != true) {
+// //     rect(340, 200, width/2, height/2);
+// //     input.position(width/2, height/2);
+// //     button.position(input.x + input.width, 65);
+// //     button.mousePressed(greet)
+// //   }
+// // }
+// //
+// // function greet(){
+// //   let name = input.value()
+// //   debugger
+// // }
+//
+//
+// ///////////CREATE MORE ENEMIES, EVERYTIME YOU RUN INTO THEM
+// function moreEnemies(sprite) {
+//
+//
+// for(let j=0; j<3; j++){
+//   let enemy = createSprite(random(0, width), random(0, height), 10, 10);
+//   enemy.shapeColor = color(50, 60, 255)
+//   enemy.addToGroup(enemies);
+// }
+//
+//   for(let j=0; j<3; j++){
+//     let sprite = createSprite(random(0, width), random(0, height), 10, 10);
+//     sprite.shapeColor = color(50, 60, 255)
+//     enemies.add(sprite);
+//   }
+//
+// }
+//
+//
+//
+// //////////WHAT HAPPENS WHEN YOU COLLECT SOMETHING YOU DON'T WANT
+//
+// function dontCollect(collector, collected) {
+//   collected.remove();
+//   moreEnemies(enemies);
+//   collector.scale -= .15;
+//
+//
+//
+//   moreEnemies(enemies);
+//
+//
+//   // collected.add();
+//   score -= 1;
+//
+// ///////if stretchy scale becomes less than -0.05000000000000007, remove it and pop up with "GAME OVER"
+// }
+//
+//
+// //////////WHAT HAPPENS WHEN YOU COLLECT SOMETHING YOU WANT
+// function collect(collector, collected) {
+//   //collector is another name for stretchy
+//   //collected is the sprite in the group collectibles that triggered
+//   //the event
+//   collected.remove();
+//   collector.scale += .15;
+//
+//   score+= 3
+// }
+//
+// //////////FALLING BLUE RAIN
+// function fallingRain(sprite) {
+//   for (var i = 0; i < sprite.length; i++) {
+//    sprite[i].position.y += sprite[i].width * 0.15;
+//    if (sprite[i].position.y > width) {
+//      sprite[i].position.y = 0;
+//    }
+//  }
+// }
+//
+// //////////SETTING UP THE KEY COMMANDS: UP, DOWN, LEFT, RIGHT, SPACE FOR FULL STOP
+// function keyPressed() {
+//   if (keyCode == RIGHT_ARROW) {
+//     stretchy.setSpeed(3, 0);
+//     // stretchy.velocity.x = (stretchy.position.x)/10;
+//   }
+//   else if (keyCode == DOWN_ARROW) {
+//     stretchy.setSpeed(3, 90);
+//
+//     // stretchy.velocity.y = (stretchy.position.y)/10;
+//   }
+//   else if (keyCode == LEFT_ARROW) {
+//     stretchy.setSpeed(3, 180);
+//     // stretchy.velocity.x = (stretchy.position.x)/10;
+//   }
+//   else if (keyCode == UP_ARROW) {
+//     stretchy.setSpeed(3, 270);
+//     // stretchy.velocity.y = (stretchy.position.y)/10;
+//   }
+//   else if (key == ' ') {
+//     stretchy.setSpeed(0, 0);
+//   }
+//   return false;
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // walls = new Group();
+// //
+// // for (var i = 0; i < 20; i++) {
+// //   var w = createSprite(
+// //     random(125, width-125), (height/5)*i,
+// //     random(10, 100), random(10, 100));
+// //   w.shapeColor = color(0);
+// //   walls.add(w);
+// // }
